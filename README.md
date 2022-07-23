a759dd9797dcd62ec7b653a68ba409d0a9afc9d5
2439905184 <suchenglong018@126.com>
2022-07-23 09:49:32 +0800

    📃 docs: 添加说明文档

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # godot-2d
 Godot引擎的2d版本（去除了3d模块和3d物理模块）提供预编译的编辑器
+# usage
+1. rename custom_xxx.py to custom.py
+2. copy one file to godot's src dir
+3. scons profile=custom.py
---
+++ custom_editor.py
@@ -0,0 +1,10 @@
+# Generated using https://godot-build-options-generator.github.io
+
+#disable_3d = "yes"
+module_arkit_enabled = "no"
+module_bullet_enabled = "no"
+module_camera_enabled = "no"
+module_csg_enabled = "no"
+module_dds_enabled = "no"
+module_gridmap_enabled = "no"
+module_mobile_vr_enabled = "no"
